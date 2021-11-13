b074cc8cf92bcb744e85c8516b93964c022fc84a
sushmitha gadde <sushmita.gadde@lotusdew.in>
2021-11-13 16:12:00 +0530

    modified test case

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Trade and bet on a variety of football betting markets, including those on the Premier League, Champions League, La Liga, Bundesliga and MLS./i);
   expect(linkElement).toBeInTheDocument();
 });
